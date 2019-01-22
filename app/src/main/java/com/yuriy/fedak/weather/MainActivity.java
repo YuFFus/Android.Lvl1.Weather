d31e7f07c8884fd9e9181e6039f29cdd909c22a4
Yuriy Fedak <fedak@sst-dv.ru>
2019-01-22 23:37:16 +1000

    Lesson 2 done!

--- app/src/main/java/com/yuriy/fedak/weather/MainActivity.java
+++ app/src/main/java/com/yuriy/fedak/weather/MainActivity.java
@@ -9,5 +9,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewUtility.makeToast(getApplicationContext(), "onCreate");
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        ViewUtility.makeToast(getApplicationContext(), "onStart");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ViewUtility.makeToast(getApplicationContext(), "onResume");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        ViewUtility.makeToast(getApplicationContext(), "onPause");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        ViewUtility.makeToast(getApplicationContext(), "onStop");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        ViewUtility.makeToast(getApplicationContext(), "onDestroy");
     }
 }
