8ee7b44fb13b2603498e49abf061230ac41bd7f6
Yuriy Fedak <fedak@sst-dv.ru>
2019-02-06 03:23:00 +1000

    Lesson 5 done w/o "*" task.

--- app/src/main/java/com/yuriy/fedak/weather/MainActivity.java
+++ app/src/main/java/com/yuriy/fedak/weather/MainActivity.java
@@ -1,6 +1,8 @@
 package com.yuriy.fedak.weather;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
@@ -12,9 +14,27 @@ import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity {
 
     public static final String CITY_NAME_EXTRA = "cityLookingFor";
+    public static final String APP_PREFERENCES = "mySettings";
+    public static final String APP_PREFERENCES_THEME = "Theme";
+    public static final String THEME_STANDARD = "Theme_Standard";
+    public static final String THEME_DARK = "Theme_Dark";
+    SharedPreferences mSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        if (mSettings.contains(APP_PREFERENCES_THEME)){
+            switch (mSettings.getString(APP_PREFERENCES_THEME,"")){
+                case THEME_STANDARD:
+                    super.setTheme(R.style.AppTheme);
+                    setTheme(R.style.AppTheme);
+                    break;
+                case THEME_DARK:
+                    super.setTheme(R.style.DarkAppTheme);
+                    setTheme(R.style.DarkAppTheme);
+                    break;
+            }
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -25,35 +45,39 @@ public class MainActivity extends AppCompatActivity {
         ImageButton buttonMenu = findViewById(R.id.imageButtonMenu);
         MenuClickListener buttonMenuClickListener = new MenuClickListener();
         buttonMenu.setOnClickListener(buttonMenuClickListener);
-
-        ViewUtility.makeToast(getApplicationContext(), "onCreate");
+/*        ViewUtility.makeToast(getApplicationContext(), "onCreate");*/
     }
     @Override
     protected void onStart(){
         super.onStart();
-        ViewUtility.makeToast(getApplicationContext(), "onStart");
+/*        ViewUtility.makeToast(getApplicationContext(), "onStart");*/
     }
     @Override
     protected void onResume() {
         super.onResume();
-        ViewUtility.makeToast(getApplicationContext(), "onResume");
+        /*ViewUtility.makeToast(getApplicationContext(), "onResume");*/
     }
     @Override
     protected void onPause(){
         super.onPause();
-        ViewUtility.makeToast(getApplicationContext(), "onPause");
+        /*ViewUtility.makeToast(getApplicationContext(), "onPause");*/
     }
     @Override
     protected void onStop(){
         super.onStop();
-        ViewUtility.makeToast(getApplicationContext(), "onStop");
+        /*ViewUtility.makeToast(getApplicationContext(), "onStop");*/
     }
     @Override
     protected void onDestroy(){
         super.onDestroy();
-        ViewUtility.makeToast(getApplicationContext(), "onDestroy");
+        /*ViewUtility.makeToast(getApplicationContext(), "onDestroy");*/
     }
 
+/*    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString();
+        super.onSaveInstanceState(outState);
+    }*/
     private class FindClickListener implements View.OnClickListener{
         @Override
         public void onClick(View view){
--- app/src/main/java/com/yuriy/fedak/weather/MenuActivity.java
+++ app/src/main/java/com/yuriy/fedak/weather/MenuActivity.java
@@ -1,17 +1,80 @@
 package com.yuriy.fedak.weather;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.RadioButton;
 
 public class MenuActivity extends AppCompatActivity {
 
+    public static final String APP_PREFERENCES = "mySettings";
+    public static final String APP_PREFERENCES_THEME = "Theme";
+    public static final String THEME_STANDARD = "Theme_Standard";
+    public static final String THEME_DARK = "Theme_Dark";
+    public String APP_PREFERENCES_THEME_VALUE;
+    SharedPreferences mSettings;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+        if (mSettings.contains(APP_PREFERENCES_THEME)){
+            switch (mSettings.getString(APP_PREFERENCES_THEME,"")){
+                case THEME_STANDARD:
+                    super.setTheme(R.style.AppTheme);
+                    setTheme(R.style.AppTheme);
+                    break;
+                case THEME_DARK:
+                    super.setTheme(R.style.DarkAppTheme);
+                    setTheme(R.style.DarkAppTheme);
+                    break;
+            }
+        }
         super.onCreate(savedInstanceState);
-        super.setTheme(R.style.DarkAppTheme);
-        getApplication().setTheme(R.style.DarkAppTheme);
         setContentView(R.layout.activity_menu);
-
+        RadioButton radioButtonStandardTheme = findViewById(R.id.radioButtonStandardTheme);
+        radioButtonStandardTheme.setOnClickListener(new RadioButtonStandardThemeClickListener());
+        RadioButton radioButtonDarkTheme = findViewById(R.id.radioButtonDarkTheme);
+        radioButtonDarkTheme.setOnClickListener(new RadioButtonDarkThemeClickListener());
+        if (mSettings.contains(APP_PREFERENCES_THEME)){
+            switch (mSettings.getString(APP_PREFERENCES_THEME,"")){
+                case THEME_STANDARD:
+                    radioButtonStandardTheme.setChecked(true);
+                    APP_PREFERENCES_THEME_VALUE = THEME_STANDARD;
+                    break;
+                case THEME_DARK:
+                    radioButtonDarkTheme.setChecked(true);
+                    APP_PREFERENCES_THEME_VALUE = THEME_DARK;
+                    break;
+            }
+        }
+        else {
+            radioButtonStandardTheme.setChecked(true);
+            APP_PREFERENCES_THEME_VALUE = THEME_STANDARD;
+        }
+/*        ViewUtility.makeToast(getApplicationContext(), "onCreate");*/
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Editor editor = mSettings.edit();
+        editor.putString(APP_PREFERENCES_THEME, APP_PREFERENCES_THEME_VALUE);
+        editor.commit();
+/*        ViewUtility.makeToast(getApplicationContext(), "onStop");*/
+    }
+    private class RadioButtonStandardThemeClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            APP_PREFERENCES_THEME_VALUE = THEME_STANDARD;
+        }
+    }
+    private class RadioButtonDarkThemeClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            APP_PREFERENCES_THEME_VALUE = THEME_DARK;
+        }
     }
 }
